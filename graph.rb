ee0d02c0ab176945186329939ec93a921d344c5e
Erik Doernenburg <erik@doernenburg.com>
2013-03-07 17:23:49 +0000

    Now uses shared code for pages files.

--- graph.rb
+++ graph.rb
@@ -1,35 +1,26 @@
 require 'json'
 
-idx = 1
-twers = {}
-File.open("twitternames.txt", "r").each_line do |line|
-  unless line.start_with?("#") or line.strip == "" then
-    twers[line.strip] = idx
-    idx += 1
+load 'shared.rb'
+
+
+def write_graph(twers, followers)
+  puts "Creator \"graph.rb on #{Time.now.to_s}\""
+  puts "graph"
+  puts "["
+
+  twers.each { |username, a| puts "  node\n  [\n    id #{a[:id]}\n    label \"#{username}\"\n    size #{a[:fcount]}\n  ]" }
+  followers.each_key { |id| puts "  node\n  [\n    id #{id}\n    label \"\"\n    size 1\n  ]" }
+
+  followers.each do |followerid, followingids|
+    followingids.each do | followingid |
+      puts "  edge\n  [\n    source #{followerid}\n    target #{followingid}\n  ]"
+    end
   end
+
+  puts "]"
 end
 
-followers = {}
-twers.each_key do |username|
-  response = JSON.parse(File.read("followers-#{username}.json"))
-  response["ids"].each do |followerid|
-    followers[followerid] ||= []
-    followers[followerid].push(twers[username])
-  end
-end
 
-
-puts "Creator \"graph.rb on #{Time.now.to_s}\""
-puts "graph"
-puts "["
-
-twers.each { |username, id| puts "  node\n  [\n    id #{id}\n    label \"#{username}\"\n  ]" }
-followers.each_key { |id| puts "  node\n  [\n    id #{id}\n    label \"\"\n  ]" }
-
-followers.each do |followerid, followingids|
-  followingids.each do | followingid |
-    puts "  edge\n  [\n    source #{followerid}\n    target #{followingid}\n  ]"
-  end
-end
-
-puts "]"
+twers = create_map(read_names())
+followers = read_followers(twers)
+write_graph(twers, followers)
